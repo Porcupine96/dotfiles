139f024837627bfb1f50101cfe3f46b72c62b2e2
Łukasz Kaźmierczak <luki.pol@gmail.com>
2024-10-24 12:40:54 +0200

    sync

---
+++ mac/link.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/mac/config/kitty ~/.config/kitty
+ln -s ~/dotfiles/mac/config/yabai ~/.config/yabai
+ln -s ~/dotfiles/mac/config/skhd ~/.config/skhd
+ln -s ~/dotfiles/mac/config/copyq ~/.config/copyq
+ln -s ~/dotfiles/mac/config/flameshot ~/.config/flameshot
+ln -s ~/dotfiles/mac/config/nvim ~/.config/nvim
+ln -s ~/dotfiles/mac/config/mpv ~/.config/mpv
+ln -s ~/dotfiles/mac/config/idasen ~/.config/idasen
+ln -s ~/dotfiles/mac/config/karabiner ~/.config/karabiner
+ln -s ~/dotfiles/mac/config/htop ~/.config/htop
+ln -s ~/dotfiles/mac/config/sketchybar ~/.config/sketchybar
